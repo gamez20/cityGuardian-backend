417c0843a1fc572c10d1ccbd46324d4e93996c50
Santiago Gamez Guerra <gamez.guerra18@gmail.com>
2025-05-23 20:17:23 -0500

    add configuration cords

--- src/main/java/co/edu/uniquindio/cityguardian/config/SecurityConfig.java
+++ src/main/java/co/edu/uniquindio/cityguardian/config/SecurityConfig.java
@@ -60,7 +60,7 @@ public class SecurityConfig {
         config.setAllowedOrigins(List.of("*"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
-        config.setAllowCredentials(true);
+        config.setAllowCredentials(false);
 
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
