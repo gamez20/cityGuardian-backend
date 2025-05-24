f1578ddeb3876a16c342124591b49777b0a7bb61
Andres-Correa69 <141978219+Andres-Correa69@users.noreply.github.com>
2025-05-24 15:05:16 -0500

    fix log report status

--- src/main/java/co/edu/uniquindio/cityguardian/services/impl/ReportServiceImpl.java
+++ src/main/java/co/edu/uniquindio/cityguardian/services/impl/ReportServiceImpl.java
@@ -215,8 +215,7 @@ public class ReportServiceImpl implements ReportService {
     public void markAsVerified(String id) throws Exception {
         Report report = repository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Reporte no encontrado"));
-        report.setStatus(ReportStatus.VERIFIED);
-        report.setRejectReason(null);
+        report.addStateChange(ReportStatus.VERIFIED, null);
         repository.save(report);
     }
 
@@ -229,8 +228,7 @@ public class ReportServiceImpl implements ReportService {
         Report report = repository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Reporte no encontrado"));
 
-        report.setStatus(ReportStatus.REJECTED);
-        report.setRejectReason(rejectReason);
+        report.addStateChange(ReportStatus.REJECTED, rejectReason);
         repository.save(report);
     }
 
@@ -238,8 +236,7 @@ public class ReportServiceImpl implements ReportService {
     public void markAsResolved(String id) throws Exception {
         Report report = repository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Reporte no encontrado"));
-        report.setStatus(ReportStatus.RESOLVED);
-        report.setRejectReason(null);
+        report.addStateChange(ReportStatus.RESOLVED, null);
         repository.save(report);
     }
 
@@ -247,11 +244,9 @@ public class ReportServiceImpl implements ReportService {
     public void sendToReview(String id) throws Exception {
         Report report = repository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Reporte no encontrado"));
-        report.setStatus(ReportStatus.CREATED);
-        report.setRejectReason(null);
+        report.addStateChange(ReportStatus.CREATED, null);
         repository.save(report);
     }
-
 
     @Override
     public List<ReportDTO> findReportsNearLocation(LocationDTO location, double radiusInKm) throws Exception {
